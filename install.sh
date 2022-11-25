c291d7b0458a723bd3fdca02edc17d0192a20950
Lotfi-Touil <lotfi.touil7@gmail.com>
2022-11-25 23:36:29 +0100

    fix init

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
+chmod -R 777 ./serv1/storage/ ./serv2/storage/
 docker-compose build
 docker-compose up -d
 # setup first app
 docker-compose exec php1 /bin/sh -c "composer install"
 docker-compose exec php1 /bin/sh -c "npm install"
-docker-compose exec php1 /bin/sh -c "npm run build"
 docker-compose exec php1 /bin/sh -c "npm run dev"
 docker-compose exec php1 php artisan config:cache
 docker-compose exec php1 php artisan key:generate
@@ -11,7 +11,6 @@ docker-compose exec php1 php artisan migrate:fresh --seed
 # setup second app
 docker-compose exec php2 /bin/sh -c "composer install"
 docker-compose exec php2 /bin/sh -c "npm install"
-docker-compose exec php2 /bin/sh -c "npm run build"
 docker-compose exec php2 /bin/sh -c "npm run dev"
 docker-compose exec php2 php artisan config:cache
 docker-compose exec php2 php artisan key:generate
